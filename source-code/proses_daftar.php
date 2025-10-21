6c54456d422f55038b0e5cb0e1a867387ac60f80
Muhammad Hasan | sanxnn <hasanmuh497@gmail.com>
2025-10-21 09:42:24 +0700

    Update link to registration form in proses_daftar.php

--- source-code/proses_daftar.php
+++ source-code/proses_daftar.php
@@ -23,7 +23,7 @@
             <?php else: ?>
             <p class="alert alert-warning">Tidak ada data yang dikirim.</p>
             <?php endif; ?>
-            <a href="form.html" class="btn btn-primary mt-3">Kembali ke Form</a>
+            <a href="form_daftar.html" class="btn btn-primary mt-3">Kembali ke Form</a>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
